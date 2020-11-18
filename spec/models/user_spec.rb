38e50b1c87162d0840d32fee81b58045c59d9c4c
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-18 19:28:29 +0500

    add specs for user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
   it "Create a new data container" do
-    @user = User.new
+    @user = User.new(name: 'abc', email: 'abc@abc.com')
     expect(@user).to be_a_new(User)
+  end
+
+  it "User is not valid without name attribute" do
+    @user = User.new(name: nil)
+    expect(@user).to_not be_valid
+  end
+
+  it "User is not valid without email attribute" do
+    @user = User.new(email: nil)
+    expect(@user).to_not be_valid
   end
 end
