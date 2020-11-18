3c1cbd84d7d86ebf724ce8dc2c45a3b9b1317504
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-18 15:05:09 +0500

    edit #new, #create and #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new
   end
 
   def craete
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
