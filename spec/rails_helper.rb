79f4cabc04797393d1c33e39035010ae6a0d61d2
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-18 18:36:04 +0500

    require capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'\
+require 'rspec/rails'
+require 'capybara'
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -11,7 +12,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
