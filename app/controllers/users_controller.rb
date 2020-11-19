fd5f1976578733ff687f627e610a378db193a9e9
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-19 13:50:23 +0500

    fix issues in new and show view template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:user_name, :email)
   end
 end
