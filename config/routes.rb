3db30cdf0a22931794e2104910366000da7a00cb
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-19 17:20:23 +0500

    change controller name to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
-  get 'login', to: 'session#new'
-  post 'login', to: 'session#create'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
