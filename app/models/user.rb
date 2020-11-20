e0c29a53b3f13dd752cd3c60ce562e8807b00555
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-20 16:18:08 +0500

    add associations to event model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   validates :user_name, :email, presence: true
   validates_uniqueness_of :user_name
 
+  has_many :invitations, foreign_key: :event_creator_id
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+  has_many :attended_events, through: :invitations, class_name: 'Event'
   def downcase
     self.user_name.downcase!
   end
