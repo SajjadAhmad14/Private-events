3952fd58d52eb9f562ec9ea5ff7546adf5621e37
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-20 16:16:35 +0500

    add associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ApplicationRecord
+  has_many :invitations, foreign_key: :created_event_id
+  has_many :attendees, through: :invitations, class_name: 'User'
+  belongs_to :creator, class_name: 'User'
 end
