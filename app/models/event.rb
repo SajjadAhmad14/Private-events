9378dfa4e44560a76bf1f20deb8cda7d4887eb11
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-21 19:37:57 +0500

    add validations to user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
+  validates :date, :name, presence: true
   has_many :invitations, foreign_key: :attended_event_id
   has_many :attendees, through: :invitations, source: :creator
   belongs_to :creator, class_name: 'User'
