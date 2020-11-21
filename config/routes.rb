cbee3a4ccfca0d06b74d4b0c6e8dbb173898e355
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-21 18:48:13 +0500

    add routes for events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
+  resources :events, only:[:new, :create, :show, :index]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
 end
