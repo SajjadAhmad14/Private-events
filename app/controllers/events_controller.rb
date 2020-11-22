cf96440fb87142f30385548fcef6ac398e589bb9
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-22 14:29:21 +0500

    events show page now shows creator name and its index page shows all the events created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,29 @@
 class EventsController < ApplicationController
   def new
-
+    @event = Event.new
   end
 
   def create
-
+    @user = User.find(session[:user_id])
+    @event = @user.created_events.new(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
   end
 
   def show
-
+    @event = Event.find(params[:id])
   end
 
   def index
-    
+    @events = Event.all
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :description)
   end
 
 end
