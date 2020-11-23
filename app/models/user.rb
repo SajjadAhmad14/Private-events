a5bc9f0cc3fdf1c70245058fa3ad2e6a891985ae
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-23 13:48:02 +0500

    edit user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   validates :user_name, :email, presence: true
   validates_uniqueness_of :user_name
 
-  has_many :invitations, foreign_key: :event_attendee_id
-  has_many :attended_events, through: :invitations, source: :attended_event
-  has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
+  has_many :event_attendences, foreign_key: :event_attendee_id
+  has_many :attended_events, through: :event_attendences, class_name: 'Event'
+  has_many :created_events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
 
 
   def downcase
