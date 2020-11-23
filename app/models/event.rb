73a53411e9074a471d697390eb449d56199c4a1e
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-23 13:47:36 +0500

    edit event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   validates :name, :description, presence: true
-  has_many :invitations, foreign_key: :attended_event_id
-  has_many :event_attendees, through: :invitations, source: :event_attendee
+  has_many :event_attendences, foreign_key: :attended_event_id
+  has_many :event_attendees, through: :event_attendences, source: :event_attendee
   belongs_to :creator, class_name: 'User'
 end
