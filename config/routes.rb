a1d2cf03da6994dabedf963fe227196856d8d914
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-23 20:30:23 +0500

    install bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'events#index'
   resources :users, only: [:new, :create, :show]
   resources :events, only:[:new, :create, :show, :index]
   get 'login', to: 'sessions#new'
