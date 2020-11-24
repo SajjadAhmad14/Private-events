04ed616b334fdb3b5e3b44791cc67504626bd2a2
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-24 16:22:40 +0500

    add a route for log out and add #index to show all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'events#index'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index]
   resources :events, only:[:new, :create, :show, :index]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
