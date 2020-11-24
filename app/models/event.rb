75d75e9e519088c3e25ef4a03323b35082425cec
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-24 20:53:17 +0500

    event index page show all upcoming and past events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name, :description, presence: true
+  validates :name, :date, :description, presence: true
 
   has_many :event_attendences, foreign_key: :attended_event_id
   has_many :event_attendees, through: :event_attendences
