d436c4a1f100861881399ae09860c3d0e7c05ce1
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-24 16:48:23 +0500

    add condition to check if session available

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,15 @@
 module SessionsHelper
 
   def current_user_name
+    if @user
     @user = User.find(session[:user_id])
     @user.user_name
+    end
+  end
+
+  def logged_in?
+    if @user
+    @user = User.find(session[:user_id])
+    end
   end
 end
