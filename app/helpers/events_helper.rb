e77109e31c8991b614946b6b21697f0333712dcf
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-25 14:27:12 +0500

    add helpers for past and present helpers for events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,12 @@ module EventsHelper
   def event_params
     params.require(:event).permit(:name, :description, :date)
   end
+
+  def past_events
+    @events.where("date < ?", Date.today)
+  end
+
+  def future_events
+    @events.where("date > ?", Date.today)
+  end
 end
