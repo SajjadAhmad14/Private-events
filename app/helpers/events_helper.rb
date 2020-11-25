29957f53d946b5e274b061833d420830cdcec699
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-25 15:13:45 +0500

    remove past_events and future_events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,12 +2,4 @@ module EventsHelper
   def event_params
     params.require(:event).permit(:name, :description, :date)
   end
-
-  def past_events
-    @events.where("date < ?", Date.today)
-  end
-
-  def future_events
-    @events.where("date > ?", Date.today)
-  end
 end
