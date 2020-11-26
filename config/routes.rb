9358ea314ab400bf3edcceb19d415ce8f5c81988
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-26 16:00:22 +0500

    add route for #create in attendees controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'events#index'
   resources :users, only: [:new, :create, :show, :index]
   resources :events, only:[:new, :create, :show, :index]
+  resources :attendees, only:[:create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
