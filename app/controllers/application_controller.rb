eb187f696f7d9d33c4623bfeed74d9721789c0fe
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-26 19:51:54 +0500

    add an helper method named past-events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :past_events
   def current_user
     session[:user_id] ? User.find(session[:user_id]) : false
   end
 
   def logged_in?
     current_user ? true : false
+  end
+  def past_events
+    Event.where("date < ?", Date.today)
   end
 end
