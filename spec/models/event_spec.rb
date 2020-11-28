51d102825e66103e6d123ddbdf61fe52f9431e8a
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-28 16:16:38 +0500

    add spec tests for User and Event models

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new(user_name:'subject')}
+  it 'Create a new data container' do
+    event = Event.new(name: 'new', date: '2020-02-02', description: 'new event')
+    expect(event).to be_a_new(Event)
+  end
+
+  it 'Event is valid if a name, descrption, and date attributes are present' do
+    event = user.created_events.new(name: 'new', date: '2020-02-02', description: 'new event', creator_id: 1)
+    expect(event).to be_valid
+  end
+
+  it 'Event is not valid without name attribute' do
+    event = user.created_events.new(name: nil)
+    expect(event).to_not be_valid
+  end
+  
+  it 'Event is not valid without date attribute' do
+    event = user.created_events.new(date: nil)
+    expect(event).to_not be_valid
+  end
+  
+  it 'Event is not valid without name attribute' do
+    event = user.created_events.new(description: nil)
+    expect(event).to_not be_valid
+  end
+  
+  it 'Event is not valid without creator_id attribute' do
+    event = user.created_events.new(creator_id: nil)
+    expect(event).to_not be_valid
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  
   it 'Create a new data container' do
-    user = User.new(name: 'abc', email: 'abc@abc.com')
+    user = User.new(user_name: 'abc')
     expect(user).to be_a_new(User)
   end
 
-  it 'User is valid if a name and email attributes are present' do
-    user = User.new(name: 'abc', email: 'abc@abc.com')
+  it 'User is valid if a name attribute is present' do
+    user = User.new(user_name: 'abc')
     expect(user).to be_valid
   end
 
+
   it 'User is not valid without name attribute' do
-    user = User.new(name: nil)
+    user = User.new(user_name: nil)
+    user.downcase
     expect(user).to_not be_valid
+  end
+  
+  it 'User name should be unique' do
+    user = User.new(user_name: 'sajjad')
+    duplicate_user = User.new(user_name: 'sajjad')
+    expect(duplicate_user.valid?).to be_falsy
   end
 
-  it 'User is not valid without email attribute' do
-    user = User.new(email: nil)
-    expect(user).to_not be_valid
-  end
 end
+
