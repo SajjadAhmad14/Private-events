143fc915ce47fb02af44c268c0fc50ec5ff4e7e8
Sajjad Ahmad <jogimar14@gmail.com>
2020-11-28 13:02:54 +0500

    add a nw helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?, :past_events
+  helper_method :current_user, :logged_in?, :signed_up?, :past_events
   def current_user
     session[:user_id] ? User.find(session[:user_id]) : false
+  end
+
+  def signed_up?
+    current_user ? true : false
   end
 
   def logged_in?
